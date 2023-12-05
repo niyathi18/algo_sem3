9074b019df86ee512b60ff48e28c5ae10049b823
niyathi18 <niyathisurthineni1801@gmail.com>
2023-12-06 00:49:37 +0530

    selection sort

---
+++ selection.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr{5,4,3,2,1};
+    int n=arr.size();
+    //selection sort
+    for(int i=0;i<n-1;i++){
+        int minIndex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIndex]){
+                //new min,store
+                minIndex=j;
+            }
+        }
+        //swap
+        swap(arr[i],arr[minIndex]);
+    }
+
+    //printing
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    return 0;
+} 
